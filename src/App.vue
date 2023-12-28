4952bced007d038d17c097891443b530251ce3c5
Alfredo Castrogiovanni <castro.alfry@gmail.com>
2023-12-28 11:13:54 +0100

    create project structure

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+    export default {
+    name: "AppVue"
+    }
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+    @use '../node_modules/bootstrap/scss/bootstrap.scss' as *;
+    @use './style/general.scss' as *;
+</style>
